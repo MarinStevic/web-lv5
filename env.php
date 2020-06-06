c13eb68a469d352478d5de4a4db1cc39337d1934
Marin <marin.stevic@gmail.com>
2020-06-06 23:46:31 +0200

    Update env.php

--- env.php
+++ env.php
@@ -4,8 +4,8 @@ namespace db;
 
 class DbConfig
 {
-    const HOST = "localhost";
-    const USER = "root";
-    const PASS = "";
-    const DB = "fighters";
+    const HOST = "eu-cdbr-west-03.cleardb.net";
+    const USER = "b3c2d33dc4de8a";
+    const PASS = "6fe86098";
+    const DB = "heroku_deb61df1227d9a4";
 }
